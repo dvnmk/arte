e2426ae458b0040435adea118b2983aa819fb1fb
dvnmk <divinomok@gmail.com>
2015-01-12 02:19:09 +0900

    unneedede dinge weg

--- get-arte.lisp
+++ get-arte.lisp
@@ -1,33 +1,31 @@
 #!/usr/local/bin/sbcl --noinform 
 
-(ql:quickload "yason") ;json
-(ql:quickload "drakma") ;curl
+(ql:quickload "yason")
+(ql:quickload "drakma")
 (setf drakma:*header-stream* nil)
-(ql:quickload "trivial-shell") ;wget
-;;(ql:quickload "cl-ppcre") ;blanko2_
 
-(defun get-json (nm)
+(defun get-json (nmr)
   (let* ((url 
          (concatenate 'string
                       "http://arte.tv/papi/tvguide/videos/stream/player/D/"
-                      nm
+                      nmr
                       "_PLUS7-D/ALL/ALL.json"))
          (vec (flexi-streams:octets-to-string (drakma:http-request url))))
     (yason:parse vec) ))
 
-(defun get-url-y-titel (jsn)
+(defun url-y-titel (jsn)
   (let ((url (alexandria:ensure-gethash "url" (alexandria:ensure-gethash "HTTP_MP4_SQ_1" (alexandria:ensure-gethash "VSR"                                                                       (alexandria:ensure-gethash "videoJsonPlayer" jsn)))))
         (titel (alexandria:ensure-gethash "VTI" (alexandria:ensure-gethash "videoJsonPlayer" jsn))))
     (list url (apo2bar (blanko2underbar titel)))))
 
-	;; QUALITY = LQ, MQ, EQ, SQ where LQ < MQ < EQ < SQ
-	;; 		SQ = 720p 1280x720 bitrate 2200 (HD)
-	;; 		EQ = 400p 720x406 bitrate 1500
-	;; 		MQ = 400p 720x406 bitrate 800
-	;; 		LQ = 220p 320x200 bitrate 300
+;; QUALITY = LQ, MQ, EQ, SQ where LQ < MQ < EQ < SQ
+;; 		SQ = 720p 1280x720 bitrate 2200 (HD)
+;; 		EQ = 400p 720x406 bitrate 1500
+;; 		MQ = 400p 720x406 bitrate 800
+;; 		LQ = 220p 320x200 bitrate 300
 
-(defun arte-info (nm)
-  (get-url-y-titel (get-json nm)))
+(defun arte-info (nmr)
+  (url-y-titel (get-json nmr)))
 
 (defun blanko2underbar (string)
   (cl-ppcre:regex-replace-all " " string "_"))
@@ -35,29 +33,23 @@
 (defun apo2bar (string)
   (cl-ppcre:regex-replace-all "'" string "-"))
 
-(defun wget (url output-name)
-  (with-open-file (my-stream (concatenate 'string output-name ".mp4")
-                             :direction :output
-                             :element-type '(unsigned-byte 8)
-                             :if-does-not-exist :create
-                             :if-exists :supersede)
-    (let ((content (drakma:http-request url)))
-      (loop for i across content do
-           (write-byte i my-stream)))))
+;; heap exhausted faulty
+;; (defun wget (url output-name)
+;;   (with-open-file (my-stream (concatenate 'string output-name ".mp4")
+;;                              :direction :output
+;;                              :element-type '(unsigned-byte 8)
+;;                              :if-does-not-exist :create
+;;                              :if-exists :supersede)
+;;     (let ((content (drakma:http-request url)))
+;;       (loop for i across content do
+;;            (write-byte i my-stream)))))
 
-(defun arte (nm)
-  (let ((res (arte-info nm)))
-    (wget (car res) (cadr res))))
+;; (defun arte (nmr)
+;;   (let ((res (arte-info nmr)))
+;;     (wget (car res) (cadr res))))
 
-(defun arte-bash (nm)
-  (let* ((res (arte-info nm))
-         (cmd (concatenate 'string "wget" " -c " " -q "(car res)
-                           " -O " (concatenate 'string (cadr res)
-                                               ".mp4"))))
-    (trivial-shell:shell-command cmd)))
-
-(defun gogo (sendung-nr)
-  (let* ((info (arte-info sendung-nr))
+(defun gogo (nmr)
+  (let* ((info (arte-info nmr))
          (url (car info))
          (titel (cadr info))
          (wget-cmd (list "-c" url "-O" (concatenate 'string titel ".mp4"))))
