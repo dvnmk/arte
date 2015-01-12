6dc0466ee8d73eca82f2291ac16ef08fa1e7eea7
dvnmk <divinomok@gmail.com>
2015-01-12 21:11:31 +0900

    file-name > (asciify) >

--- arte.lisp
+++ arte.lisp
@@ -4,6 +4,10 @@
 (ql:quickload "drakma")
 (setf drakma:*header-stream* nil)
 
+
+(defmacro no-quote (etwas) 
+  `(symbol-name ',etwas))
+
 (defun nmr2json (nmr)
   (let* ((url 
          (concatenate 'string
@@ -37,33 +41,35 @@
 ;; kurz datum
 ;; (ALEXANDRIA:ensure-gethash "VS5" (VALUE-AUS-JSON foojson "VST"))
 
-(defun info (key)
-  (alexandria:ensure-gethash key *nivo*))
+(defun info (key tbl)
+  (alexandria:ensure-gethash key tbl))
 
 (defun arte-info (nmr)
-  (defparameter *nivo* (alexandria:ensure-gethash "videoJsonPlayer"(nmr2json nmr)))
-  (format t "~&* Titel : ~S" (info "VTI"))
-  (format t "~&* Aired : ~S" (info "VDA"))
-  (format t "~&* Bis   : ~S" (info "VRU"))
-  (format t "~&* Kurz.Bes : ~S" (info "V7T"))
-  (format t "~&* Bes : ~S" (info "VDE"))
-  (format t "~&* Modes : ~S" (alexandria:hash-table-keys (info "VSR")))
-  )
+  (let ((nivo (alexandria:ensure-gethash "videoJsonPlayer"(nmr2json nmr))))
+    (format t "~&* TITEL : ~S" (info "VTI" nivo))
+    (format t "~&* AIRED : ~S" (info "VDA" nivo))
+    (format t "~&* BIS   : ~S" (info "VRU" nivo))
+    (format t "~&* WAS   : ~S" (info "V7T" nivo))
+    (format t "~&* INFO  : ~S" (info "infoProg" nivo))
+    (format t "~&* BES   : ~S" (info "VDE" nivo))
+    (format t "~&* MODES : ~S" (alexandria:hash-table-keys (info "VSR" nivo )))))
 
 (defun arte-get (nmr)
-  (defparameter *nivo* (alexandria:ensure-gethash "videoJsonPlayer" (nmr2json nmr)))
-  (let* ((url (alexandria:ensure-gethash "url" (alexandria:ensure-gethash "HTTP_MP4_SQ_1" (info "VSR"))))
-         (kurz-datum (alexandria:ensure-gethash "VS5" (info "VST")))
-         (file-name (concatenate 'string
-                                 (apo2bar (blanko2underbar (info "VTI")))
-                                 kurz-datum
-                                 ".mp4"))
+  (let* ((nivo (alexandria:ensure-gethash "videoJsonPlayer" (nmr2json nmr)))
+         (url (alexandria:ensure-gethash "url" (alexandria:ensure-gethash "HTTP_MP4_SQ_1" (info "VSR" nivo))))
+         (kurz-datum (alexandria:ensure-gethash "VS5" (info "VST" nivo)))
+         (file-name (ASCIIFY
+                     (concatenate 'string
+                                  (apo2bar (blanko2underbar (info "VTI" nivo)))
+                                  kurz-datum
+                                  ".mp4")))
          (wget-cmd (list "-c" url "-O" file-name)))
     (format t "~& ~A" url)
     (format t "~& =>")
     (format t "~& ~A" file-name)
-    (princ wget-cmd)
-    (sb-ext:run-program "/usr/local/bin/wget" wget-cmd :wait nil)))
+    (format t "~& ~A" wget-cmd)
+    ;; (sb-ext:run-program "/usr/local/bin/wget" wget-cmd :wait nil)
+    ))
 
 ;; heap exhausted faulty
 ;; (defun wget (url output-name)
@@ -84,6 +90,13 @@
 ;; ** TODO fur shell / clisp, sbcl
 ;; ** TODO Unicode suppport als file-name
 
+#+:SBCL
+(defun cwd (dir)
+  (sb-posix:chdir dir))
 
-(defmacro no-quote (string)
-  `(symbol-name ',string))
+(defun getcwd ()
+  #+SBCL (sb-unix:posix-getcwd)
+  #+CCL (current-directory))
+
+;;(load "asciify.lisp")
+
