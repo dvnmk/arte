3e43fc56cca08d2c1e968c285a257757a7d1c30c
dvnmk <divinomok@gmail.com>
2015-01-14 14:25:09 +0900

    weg asciify, ccl no funzkt. wget --progress:dot:mega

--- arte.lisp
+++ arte.lisp
@@ -3,7 +3,7 @@
 (ql:quickload "yason")
 (ql:quickload "drakma")
 (setf drakma:*header-stream* nil)
-
+(load #P"~/arte/asciify.lisp")
 
 (defun nmr2json (nmr)
   (let* ((url 
@@ -34,21 +34,21 @@
   (let* ((nivo-0 (alexandria:ensure-gethash "videoJsonPlayer" (nmr2json nmr)))
         (url (alexandria:ensure-gethash "url" (alexandria:ensure-gethash "HTTP_MP4_SQ_1" (info "VSR" nivo-0))))
         (kurz-datum (alexandria:ensure-gethash "VS5" (info "VST" nivo-0)))
-        (file-name (ASCIIFY
-                     (concatenate 'string
-                                  (apo2bar (blanko2underbar (info "VTI" nivo-0)))
-                                  "-" kurz-datum ".mp4")))
-        (raw-cmd  (concatenate 'string  "wget -c " url " -O " file-name))
-        )
+         (file-name
+          (ASCIIFY (concatenate 'string
+                                (apo2bar (blanko2underbar (info "VTI" nivo-0)))
+                                "-" kurz-datum ".mp4")))
+         (raw-cmd  (concatenate 'string  "wget -c " url " -O " file-name))
+         )
     (format t "~&* TITEL : ~S" (info "VTI" nivo-0))
     (format t "~&* AIRED : ~A - ~A" (info "VDA" nivo-0)
             (info "VRU" nivo-0))
-b    (format t "~&* CASE  : ~A" (info "caseProgram" nivo-0))
+    (format t "~&* CASE  : ~A" (info "caseProgram" nivo-0))
     (format t "~&* INFO  : ~A" (info "infoProg" nivo-0))
     (format t "~&* KURZ  : ~S" (ASCIIFY (info "V7T" nivo-0)))
     (format t "~&* BES   : ~S" (info "VDE" nivo-0))
     (format t "~&* MODES : ~A" (alexandria:hash-table-keys (info "VSR" nivo-0 )))
-    (format t "~&* WGET  :")
+    (format t "~&* CMD  :")
     (format t "~& ~A" raw-cmd)    ))
 
 (defun arte-get (nmr)
@@ -59,13 +59,13 @@ b    (format t "~&* CASE  : ~A" (info "caseProgram" nivo-0))
                      (concatenate 'string
                                   (apo2bar (blanko2underbar (info "VTI" nivo-0)))
                                   "-" kurz-datum ".mp4")))
-         (wget-cmd (list "-c" url "-O" file-name))
-         (raw-cmd  (concatenate 'string  "wget -c " url " -O " file-name)))
+         (setf raw-cmd  (concatenate 'string  "wget --progress=dot:mega -c " url " -O " file-name))
+         )
     (format t "~& ~A" url)
     (format t "~& =>")
     (format t "~& ~A" file-name)
-    ;(format t "~& ~A" raw-cmd)
-    (sb-ext:run-program "/usr/local/bin/wget" wget-cmd :wait nil)
+    (format t "~& ~A" raw-cmd)
+;    (run-program "/usr/local/bin/wget" raw-cmd :wait nil)
     ))
 
 (defmacro arte-info-m (nmr6-nmr3) 
@@ -94,8 +94,9 @@ b    (format t "~&* CASE  : ~A" (info "caseProgram" nivo-0))
 ;; ** DONE filename zv datum.
 ;; ** TODO fur shell / clisp, sbcl
 ;; ** TODO Unicode suppport als file-name
+(defun ASCIIFY (x)
+  "bypass"
+  x)
+(cwd #P"~/Desktop")
 
-(cwd "/Users/dvnmk/Desktop")
-
-;;(load "asciify.lisp")
-
+(car)
