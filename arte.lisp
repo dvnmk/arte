94f3cafc00e061a2c95c68a849488b5d217602d0
dvnmk <divinomok@gmail.com>
2015-01-19 12:01:57 +0900

    apo22bar

--- arte.lisp
+++ arte.lisp
@@ -67,10 +67,10 @@
   (cl-ppcre:regex-replace-all "'" string "-"))
 
 (defun apo22bar (string)
-  (cl-ppcre:regex-replace-all #\’ string "-"))
+  (cl-ppcre:regex-replace-all "’" string "-"))
 
 (defun slash2bar (string)
-  (cl-ppcre:regex-replace-all "/" string "-"))
+  (cl-ppcre:regex-replace-all "/" string "S"))
 
 (defun info (key tbl)
   (alexandria:ensure-gethash key tbl))
@@ -93,11 +93,15 @@
          (kurz-datum (subseq (alexandria:ensure-gethash "VS5" (info "VST" nivo-0))
                              0 4))
          (file-name (concatenate 'string
-                                 (slash2bar (apo22bar (apo2bar (blanko2underbar (info "VTI" nivo-0)))))
+                                 (slash2bar
+                                  (apo22bar
+                                   (apo2bar (blanko2underbar (info "VTI" nivo-0)))))
                                  "+" kurz-datum
                                  "~" (info "genre" nivo-0)))
          (log-name (concatenate 'string
-                                (slash2bar (apo22bar (apo2bar (blanko2underbar (info "VTI" nivo-0)))))
+                                (slash2bar
+                                 (apo22bar
+                                  (apo2bar (blanko2underbar (info "VTI" nivo-0)))))
                                 "+" kurz-datum
                                 "=" (blanko2underbar (info "V7T" nivo-0))))
          (url-simple-string (format nil "~A" url))  ;base-string 2 simple-base-string!
