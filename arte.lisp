00f5ad4a98a10f70751b166bcf81b2771a189456
dvnmk <divinomok@gmail.com>
2015-02-06 18:03:18 +0900

    log-p weg, concatenate -> format, -o log -> -a log ; append

--- arte.lisp
+++ arte.lisp
@@ -51,113 +51,75 @@
       dir))))
 
 (defun nmr2json (nmr)
-  (let* ((json-url 
-         (concatenate 'string
-                      "http://arte.tv/papi/tvguide/videos/stream/player/D/"
-                      nmr
-                      "_PLUS7-D/ALL/ALL.json"))
+  (let* ((json-url (concatenate 'string
+                                "http://arte.tv/papi/tvguide/videos/stream/player/D/"
+                                nmr
+                                "_PLUS7-D/ALL/ALL.json"))
          (vec (flexi-streams:octets-to-string (drakma:http-request json-url)
                                               :external-format :utf-8)))
-    (yason:parse vec) ))
-
-(defun blanko2underbar (string)
-  (cl-ppcre:regex-replace-all " " string "_"))
-
-(defun apo2bar (string)
-  (cl-ppcre:regex-replace-all "'" string "-"))
-
-(defun apo22bar (string)
-  (cl-ppcre:regex-replace-all "’" string "-"))
-
-(defun slash2.s (string)
-  (cl-ppcre:regex-replace-all "/" string ".s"))
+    (yason:parse vec)))
 
 (defun normalisierung (string)
+  (labels ((slash2.s (x)
+             (cl-ppcre:regex-replace-all "/" x ".s"))
+           (apo22bar (x)
+             (cl-ppcre:regex-replace-all "’" x "-"))
+           (apo2bar (x)
+             (cl-ppcre:regex-replace-all "'" x "-"))
+           (blanko2underbar (x)
+             (cl-ppcre:regex-replace-all " " x "_"))))
   (slash2.s (apo22bar (apo2bar  (blanko2underbar string)))))
 
 (defun info (key tbl)
   (alexandria:ensure-gethash key tbl))
 
 (defun arte-info (nmr)
-  (let* ((nivo-0 (alexandria:ensure-gethash "videoJsonPlayer" (nmr2json nmr)))
-         (url (alexandria:ensure-gethash "url"
-                                         (alexandria:ensure-gethash "HTTP_MP4_SQ_1"
-                                                                    (info "VSR" nivo-0))))
-         (kurz-datum (subseq (alexandria:ensure-gethash "VS5" (info "VST" nivo-0))
+  (let* ((nivo-0 (info "videoJsonPlayer" (nmr2json nmr)))
+         (url (info "url" (info "HTTP_MP4_SQ_1" (info "VSR" nivo-0))))
+         (kurz-datum (subseq (info "VS5" (info "VST" nivo-0))
                              0 4))
          (file-name (concatenate 'string
                                  (normalisierung (info "VTI" nivo-0))
                                  "+" kurz-datum
-                                 "~" (info "genre" nivo-0)))
-         )
+                                 "~" (info "genre" nivo-0))))
     (format t "~&* TITL : ~S" (info "VTI" nivo-0))
     (format t "~&* KURZ : ~S" (info "V7T" nivo-0))
     (format t "~&* INFO : ~A ~A" (info "genre" nivo-0) (info "infoProg" nivo-0))
     (format t "~&* AIRD : ~A - ~A" (info "VDA" nivo-0)(info "VRU" nivo-0))
     (format t "~&* BESS : ~A" (info "VDE" nivo-0))
-    (format t "~&* FILE : ~%  wget -c ~A -O ~A.mp4 ~%" url file-name)
+    (format t "~&* FILE : ~%  wget -c ~A -O ~A.mp4 --tries=4 ~%" url file-name)
     ;;(format t "~&* MODES : ~A" (alexandria:hash-table-keys (info "VSR" nivo-0 )))
     t))
 
-(defun arte-nimm (nmr &optional log-p)
-  (let* ((nivo-0 (alexandria:ensure-gethash "videoJsonPlayer" (nmr2json nmr)))
-         (url (alexandria:ensure-gethash "url"
-                                         (alexandria:ensure-gethash "HTTP_MP4_SQ_1"
-                                                                          (info "VSR" nivo-0))))
-         (kurz-datum (subseq (alexandria:ensure-gethash "VS5" (info "VST" nivo-0))
+(defun arte-nimm (nmr)
+  (let* ((nivo-0 (info "videoJsonPlayer" (nmr2json nmr)))
+         (url (info "url" (info "HTTP_MP4_SQ_1" (info "VSR" nivo-0))))
+         (kurz-datum (subseq (info "VS5" (info "VST" nivo-0))
                              0 4))
          (file-name (concatenate 'string
                                  (normalisierung (info "VTI" nivo-0))
                                  "+" kurz-datum
                                  "~" (info "genre" nivo-0)))
-         (log-name (cond ((null log-p)(concatenate 'string
-                                                   (normalisierung (info "VTI" nivo-0))
-                                                    "+" kurz-datum
-                                                    "=" (normalisierung (info "V7T" nivo-0))))
-                         ((zerop log-p) file-name)
-                         (t 'ERR)))
-         (url-simple-string (format nil "~A" url))  ;base-string 2 simple-base-string!
-         (wget-cmd (concatenate 'string
-                                "wget -c " url-simple-string
-                                " -O " file-name ".mp4"
-                                ;;" --progress=dot:giga "
-                                " --no-verbose "
-                                " -o " log-name ".txt"
-                                " --tries=4")))
-    (run-program "/bin/sh"
-                 (list "-c" wget-cmd)
+         (cmd (format nil "wget -c ~A -O ~A.mp4 --no-verbose -a ~A.txt --tries=4"
+                           url file-name file-name)))
+    (run-program "/bin/sh" (list "-c" cmd)
                  :wait nil
                  :output *standard-output*)))
 
 (defun arte-guck (nmr)
-  (let* ((nivo-0 (alexandria:ensure-gethash "videoJsonPlayer" (nmr2json nmr)))
-         (url (alexandria:ensure-gethash "url"
-                                         (alexandria:ensure-gethash "HTTP_MP4_SQ_1"
-                                                                          (info "VSR" nivo-0))))
-         (url-simple-string (format nil "~A" url))  ;base-string 2 simple-base-string!
-         (cmd (concatenate 'string
-                           "mplayer -really-quiet -cache 10240 "
-                           url-simple-string
-                           )))
-    (run-program "/bin/sh"
-                 (list "-c" cmd)
+  (let* ((nivo-0 (info "videoJsonPlayer" (nmr2json nmr)))
+         (url (info "url" (info "HTTP_MP4_SQ_1" (info "VSR" nivo-0))))
+         (cmd (format nil "mplayer -really-quiet -cache 10240 ~A" url)))
+    (run-program "/bin/sh" (list "-c" cmd)
                  :wait nil
                  :output *standard-output*)))
 
 (defun arte-quck (nmr)
   "arte-guck quicktime player ver."
-  (let* ((nivo-0 (alexandria:ensure-gethash "videoJsonPlayer" (nmr2json nmr)))
-         (url (alexandria:ensure-gethash "url"
-                                         (alexandria:ensure-gethash "HTTP_MP4_SQ_1"
-                                                                          (info "VSR" nivo-0))))
-         (url-simple-string (format nil "~A" url))  ;base-string 2 simple-base-string!
-         (cmd (concatenate 'string
-                           "open -a Quicktime\\ Player"
-                           " "
-                           url-simple-string
-                           )))
-    (run-program "/bin/sh"
-                 (list "-c" cmd)
+  (let* ((nivo-0 (info "videoJsonPlayer" (nmr2json nmr)))
+         (url (info "url" (info "HTTP_MP4_SQ_1" (info "VSR" nivo-0))))
+         (cmd (format nil "open -a Quicktime\\ Player ~A" url)))
+    (run-program "/bin/sh" (list "-c" cmd)
                  :wait nil
                  :output *standard-output*)))
 
@@ -165,9 +127,9 @@
   `(let ((nmr  (symbol-name ',nmr-raw)))
      (arte-info nmr)))
 
-(defmacro n (nmr-raw &optional log-p)
+(defmacro n (nmr-raw)
   `(let ((nmr (symbol-name ',nmr-raw)))
-     (arte-nimm nmr ,log-p)))
+     (arte-nimm nmr)))
 
 (defmacro g (nmr-raw)
   `(let ((nmr (symbol-name ',nmr-raw)))
@@ -187,3 +149,27 @@
                :output *standard-output*))
 
 
+;; ;;TODO
+;; (format nil "~4,'0d - ~2,'0d - ~2,'0d" 2005 6 10)
+;; "2005 - 06 - 10"
+
+;; ;; prompt
+;; (defun prompt-read (prompt)
+;;   (format *query-io* "~A: " prompt)
+;;   (force-output *query-io*)
+;;   (read-line *query-io*))
+
+
+;; (defun add-cds ()
+;;   (loop (add-record (prompt-for-cd))
+;;      (if (not (y-or-n-p "Another? [y/n]: "))
+;;          (return))))
+
+
+
+;; (defun save-db (filename)
+;;   (with-open-file (out filename
+;;                        :direction :output
+;;                        :if-exists :supersede)
+;;     (with-standard-io-syntax
+;;       (print *db* out))))
